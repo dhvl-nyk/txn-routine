b410ff15768adbd24d6142a9b35da585e3cd82de
dhvl-nyk <diablo82.26@gmail.com>
2024-08-03 04:31:52 +0530

    adding main

---
+++ src/main/java/org/example/TxnRoutineApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxnRoutineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TxnRoutineApplication.class, args);
+    }
+}
